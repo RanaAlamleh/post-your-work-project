02f509d8724decd5bd90973df7b6d396ba5c7a0a
Rana Alamleh <rana.alamleh2006@gmail.com>
2025-12-28 15:46:43 +0000

    Add a farewell message

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -190,6 +190,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
+    print("I'll see you later🖐️🎉") 
 
 
 if __name__ == "__main__":
